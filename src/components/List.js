9f14ea6a6038f62499a838b71b8eabad92e903e7
samarth2534@gmail.com <samarth2534@gmail.com>
2025-02-19 10:10:04 +0530

    Done with each folder manages its own state

--- src/components/List.js
+++ src/components/List.js
@@ -1,20 +1,30 @@
 import React, { useState } from "react";
 
 const List = ({ list }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState({});
 
   return (
     <div className="container">
       {list.map((node) => (
-        <div key={node.id}>
+        <div key={node.id} >
           {node.isFolder && (
-            <span onClick={() => setIsExpanded((prev) => !prev)}>
-              {isExpanded ? "- " : "+ "}
+            <span
+              onClick={() =>
+                setIsExpanded((prev) => ({
+                  ...prev,
+                  [node.name]: !prev[node.name],
+                }))
+              }
+            
+            >
+              {isExpanded[node.name] ? "📂 " : "📁 "}
             </span>
           )}
-          <span>{node.name}</span>
+          <span> {node.name}</span>
 
-          {isExpanded && node.children && <List list={node.children} />}
+          {isExpanded[node.name] && node.children && (
+            <List list={node.children} />
+          )}
         </div>
       ))}
     </div>
