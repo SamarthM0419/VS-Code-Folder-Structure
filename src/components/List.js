9a961d6bce64e355c5dd4a154acd56d436feb586
samarth2534@gmail.com <samarth2534@gmail.com>
2025-02-19 10:02:39 +0530

    Expandable and collapse feature added

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const List = ({ list }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <div className="container">
       {list.map((node) => (
         <div key={node.id}>
+          {node.isFolder && (
+            <span onClick={() => setIsExpanded((prev) => !prev)}>
+              {isExpanded ? "- " : "+ "}
+            </span>
+          )}
           <span>{node.name}</span>
-          {node.children && <List list={node.children} />}
+
+          {isExpanded && node.children && <List list={node.children} />}
         </div>
       ))}
     </div>
